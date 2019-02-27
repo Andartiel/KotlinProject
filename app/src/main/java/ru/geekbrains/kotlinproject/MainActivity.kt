9eb80eb9600a55426350a7f72d5da1fc862148aa
Ivan <andartiel@mail.ru>
2019-02-27 10:31:32 +0000

    Lessson_1

---
+++ app/src/main/java/ru/geekbrains/kotlinproject/MainActivity.kt
@@ -0,0 +1,21 @@
+package ru.geekbrains.kotlinproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private var buttonTest: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        buttonTest = buttonTest!!.findViewById<View>(R.id.buttonTest) as Button
+
+        bu
+        ttonTest!!.setOnClickListener { Toast.makeText(this@MainActivity, R.string.toast_hello, Toast.LENGTH_SHORT).show() }
+    }
+}
